29bfea0913569edcb1ffa4438d54828918fc104d
trippytea <James_zx2@live.com>
2022-09-21 16:51:39 -0500

    fixed tabbing again....

--- cajunrush.sql
+++ cajunrush.sql
@@ -1,8 +1,8 @@
 
 CREATE TABLE Admins (
-    	adminId int NOT NULL AUTO_INCREMENT,
-    	username VARCHAR(60) NOT NULL,
-    	password CHAR(60) NOT NULL,
+    adminId int NOT NULL AUTO_INCREMENT,
+    username VARCHAR(60) NOT NULL,
+    password CHAR(60) NOT NULL,
    	PRIMARY KEY (adminId)
 ); 
 
@@ -13,39 +13,39 @@ CREATE TABLE Coach (
 ); 
 
 CREATE TABLE Team (
-    	teamId int NOT NULL AUTO_INCREMENT,
-    	teamName VARCHAR(60) NOT NULL,
-    	coachId int NOT NULL,
+    teamId int NOT NULL AUTO_INCREMENT,
+    teamName VARCHAR(60) NOT NULL,
+    coachId int NOT NULL,
 	division VARCHAR(4) NOT NULL,
 	location VARCHAR(30 NOT NULL,
-    	PRIMARY KEY (teamId),
-    	FOREIGN KEY (coachId) REFERENCES Coach(coachId)
+    PRIMARY KEY (teamId),
+    FOREIGN KEY (coachId) REFERENCES Coach(coachId)
 ); 
 
 CREATE TABLE Park (
-    	parkId int NOT NULL AUTO_INCREMENT,
+    parkId int NOT NULL AUTO_INCREMENT,
 	parkName VARCHAR(60) NOT NULL,
-    	PRIMARY KEY (parkId)
+    PRIMARY KEY (parkId)
 ); 
 
 CREATE TABLE Field (
    	fieldId int NOT NULL AUTO_INCREMENT,
-    	fieldNum int NOT NULL,
-    	parkId int NOT NULL,
+    fieldNum int NOT NULL,
+    parkId int NOT NULL,
 	lights TINYINT(1) NOT NULL,
-    	PRIMARY KEY (fieldId),
-    	FOREIGN KEY (parkId) REFERENCES Park(parkId)
+    PRIMARY KEY (fieldId),
+    FOREIGN KEY (parkId) REFERENCES Park(parkId)
 ); 
 
 CREATE TABLE Practice (
-    	practiceId int NOT NULL AUTO_INCREMENT,
+    practiceId int NOT NULL AUTO_INCREMENT,
 	fieldId int NOT NULL,
 	fieldSection CHAR(6),
 	teamId int NOT NULL,
 	startTime TIME NOT NULL,
 	endTime TIME NOT NULL,
 	adminId int NOT NULL,
-    	PRIMARY KEY (practiceId),
+    PRIMARY KEY (practiceId),
 	FOREIGN KEY (fieldId) REFERENCES Field(fieldId),
 	FOREIGN KEY (teamId) REFERENCES Team(teamId),
 	FOREIGN KEY (adminId) REFERENCES Admins(adminId)
