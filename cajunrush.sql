f6411d713e4fe905d232c45f61d3db9e7489f2bd
trippytea <68095545+trippytea@users.noreply.github.com>
2022-09-21 16:10:44 -0500

    fixed the tabbing
    made all the attributes line up better

--- cajunrush.sql
+++ cajunrush.sql
@@ -1,53 +1,53 @@
 
 CREATE TABLE Admins (
-    adminId int NOT NULL AUTO_INCREMENT,
-    username VARCHAR(60) NOT NULL,
-    password CHAR(60) NOT NULL,
-    PRIMARY KEY (adminId)
+    	adminId int NOT NULL AUTO_INCREMENT,
+    	username VARCHAR(60) NOT NULL,
+    	password CHAR(60) NOT NULL,
+   	PRIMARY KEY (adminId)
 ); 
 
 CREATE TABLE Coach (
-    coachId int NOT NULL AUTO_INCREMENT,
+	coachId int NOT NULL AUTO_INCREMENT,
 	coachName VARCHAR(60) NOT NULL,
-    PRIMARY KEY (coachId)
+   	PRIMARY KEY (coachId)
 ); 
 
 CREATE TABLE Team (
-    teamId int NOT NULL AUTO_INCREMENT,
-    teamName VARCHAR(60) NOT NULL,
-    coachId int,
-	division VARCHAR(4),
-	location VARCHAR(30),
-    PRIMARY KEY (teamId),
-    FOREIGN KEY (coachId) REFERENCES Coach(coachId)
+    	teamId int NOT NULL AUTO_INCREMENT,
+    	teamName VARCHAR(60) NOT NULL,
+    	coachId int NOT NULL,
+	division VARCHAR(4) NOT NULL,
+	location VARCHAR(30 NOT NULL,
+    	PRIMARY KEY (teamId),
+    	FOREIGN KEY (coachId) REFERENCES Coach(coachId)
 ); 
 
 CREATE TABLE Park (
-    parkId int NOT NULL AUTO_INCREMENT,
+    	parkId int NOT NULL AUTO_INCREMENT,
 	parkName VARCHAR(60) NOT NULL,
-    PRIMARY KEY (parkId)
+    	PRIMARY KEY (parkId)
 ); 
 
 CREATE TABLE Field (
-    fieldId int NOT NULL AUTO_INCREMENT,
-    fieldNum int NOT NULL,
-    parkId int NOT NULL,
+   	fieldId int NOT NULL AUTO_INCREMENT,
+    	fieldNum int NOT NULL,
+    	parkId int NOT NULL,
 	lights TINYINT(1) NOT NULL,
-    PRIMARY KEY (fieldId),
-    FOREIGN KEY (parkId) REFERENCES Park(parkId)
+    	PRIMARY KEY (fieldId),
+    	FOREIGN KEY (parkId) REFERENCES Park(parkId)
 ); 
 
 CREATE TABLE Practice (
-    practiceId int NOT NULL AUTO_INCREMENT,
-    parkId int NOT NULL,
+    	practiceId int NOT NULL AUTO_INCREMENT,
+    	parkId int NOT NULL,
 	fieldId int NOT NULL,
 	fieldSection CHAR(6),
 	teamId int NOT NULL,
 	startTime TIME NOT NULL,
 	endTime TIME NOT NULL,
 	adminId int NOT NULL,
-    PRIMARY KEY (practiceId),
-    FOREIGN KEY (parkId) REFERENCES Park(parkId),
+    	PRIMARY KEY (practiceId),
+    	FOREIGN KEY (parkId) REFERENCES Park(parkId),
 	FOREIGN KEY (fieldId) REFERENCES Field(fieldId),
 	FOREIGN KEY (teamId) REFERENCES Team(teamId),
 	FOREIGN KEY (adminId) REFERENCES Admins(adminId)
