3b723956fff28c66b514a8ae7966e2507e3618a7
trippytea <James_zx2@live.com>
2022-09-27 01:33:08 -0500

    php to insert into DB
    added the appropriate PHP to insert into the team table when the add button is clicked.
    
    still needed are stylings, and error messages

--- create-teams.php
+++ create-teams.php
@@ -7,13 +7,59 @@ include 'admin-nav.php';
 <html>
     <body>
     <?php
+$error = false;
+if (isset($_POST['submit'])){
+
+$link = mysqli_connect("localhost",
+            "root", "", "cajun_rush_schedule");
+
+if($link === false){
+    die("ERROR: Could not connect. "
+          . mysqli_connect_error());
+}
+
+$required = array('teamName', 'ageGroup', 'teamLocation');
+
+foreach($required as $field) {
+  if (empty($_POST[$field])) {
+    $error = true;
+  }
+}
+
+if ($error) {
+} else {
+	$error= false;
+
+$teamName= mysqli_real_escape_string(
+    $link, $_REQUEST['teamName']);
+$teamLocation = mysqli_real_escape_string(
+    $link, $_REQUEST['teamLocation']);
+$ageGroup = mysqli_real_escape_string(
+    $link, $_REQUEST['ageGroup']);
+$seasonSql = "SELECT * FROM season WHERE seasonStatus=1";
+$seasonResult = mysqli_query($link, $seasonSql);
+
+$activeSeason = $seasonResult->fetch_array()[0] ?? '';
+$sql = "INSERT INTO team (teamName, teamLocation, ageGroup, seasonId)
+        VALUES ('$teamName', '$teamLocation', '$ageGroup', '$activeSeason')";
+if(mysqli_query($link, $sql)){
+    ;
+} else{
+    echo "ERROR: data not sent!!!";
+}
+
+mysqli_close($link);
+} 
+}
+?>
+    <?php
         # start of the form, the current action is create-teams.php
         echo "<form id='createteams' action='create-teams.php' method='POST'>";
         echo "<br><br>";
         echo "<span> Team Name </span><br>";
-        <input class='teamname' type='text' id='teamname'
+        echo "<input class='teamName' type='text' id='teamName' name='teamName'
         placeholder='Enter a team name'>
-        <br><br>
+        <br><br>";
 
             # the database connection 
             $conn = new mysqli('localhost', 'root', '', 'cajun_rush_schedule') 
