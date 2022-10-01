816643fcf1be5ccbb9b628f9bc6875c1f76cbaba
Alex Gachassin <alexgachassin@gmail.com>
2022-10-01 11:40:01 -0500

    added script tag to collapse nav bar

--- create-teams.php
+++ create-teams.php
@@ -138,11 +138,10 @@ echo "<body>";
             .$insertSuccess.
         "</div>";
     echo "</div>";
-?> 
+    ?> 
+    <!--php ends -->
 
     <!-- Bootstrap JS Bundle with Popper ***needed for navbar collapse*** -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
