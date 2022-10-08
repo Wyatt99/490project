91f424faa58536608e9b41799cc77de49c492e6a
Alex Gachassin <alexgachassin@gmail.com>
2022-10-08 14:10:32 -0500

    progress on form inputs

--- ysc-scheduler.php
+++ ysc-scheduler.php
@@ -3,8 +3,8 @@ include 'db.php'; //connect to database
 include 'admin-nav.php';
 ensure_logged_in();
 
-const parkID = 2;
-const parkName = "Youngsville Sports Complex";
+$parkId = 2;
+$parkName = "Youngsville Sports Complex";
 
 
 ?>
@@ -35,18 +35,108 @@ const parkName = "Youngsville Sports Complex";
 
 <body>
 	<h1 class="mt-4 mb-2 text-lg-start text-center">Youngsville Sports Complex</h1>
-	<h3 class="subText text-lg-start text-center">Practice Scheduler</h3>
+	<h3 class="subText text-lg-start text-center ">Practice Scheduler</h3>
 
-	<form method="post" class="registerForm">
-		<div class='row'>
-			<div class='col-lg-6 col-sm-12'> <!--display 2 columns on md and up, 1 column on small-->
+	<form method="GET" class="yscScheduleForm">
+    <div class="row align-items-center g-3 mt-1">
+
+        <div class="col-lg-auto col-12">
+			<label for="ageGroup" class="form-label"><strong>Age Group</strong></label>
+				<select class="form-control " id="ageGroup" style="appearance:listbox;">
+				<option value="" disabled selected hidden>Select</option> 
+				<?=ageGroupSelect($db, $parkId)?>
+				</select>
+        </div>
+
+        <div class="col-lg-auto col-12 ">
 			<div class="form-group">
-				<label for="field">Example select</label>
-				<select class="form-control" id="field">
+			<label for="field" class="form-label"><strong>Field</strong></label>
+				<select class="form-control" id="field" style="appearance:listbox; ">
+				<option value="" disabled selected hidden>Select</option> 
+				<?=fieldSelect($db, $parkId)?>
+				</select>
 			</div>
-		</div>
-	</form>
-	
+        </div>
+
+
+		<!--start time-->
+		<p class="mb-0 mb-lg-2 mt-3" style="display:inline"><strong>Start Time</strong></p>
+			<div class="col-lg-auto col-12 mt-0">
+			<select class="form-control mt-2 mt-lg-0" id='startHr' style="appearance:listbox;">
+				<option value="" disabled selected hidden>Hour</option> 
+				<option value='1'>01</option>
+				<option value='2'>02</option>
+				<option value='3'>03</option>
+				<option value='4'>04</option>
+				<option value='5'>05</option>
+				<option value='6'>06</option>
+				<option value='7'>07</option>
+				<option value='8'>08</option>
+				<option value='9'>09</option>
+				<option value='10'>10</option>
+				<option value='11'>11</option>
+				<option value='12'>12</option>
+			</select>
+			</div>
+
+			<div class="col-lg-auto col-12 mt-0">
+			<select class="form-control mt-2 mt-lg-0" id='startMin' style="appearance:listbox;">
+				<option value="" disabled selected hidden>Minute</option> 
+				<option value='00'>00</option>
+				<option value='15'>15</option>
+				<option value='30'>30</option>
+				<option value='45'>45</option>
+			</select>
+			</div>
+
+			<div class="col-lg-auto col-12 mt-0">
+			<select class="form-control mt-2 mt-lg-0" id='startAM-PM' style="appearance:listbox;">
+				<option value="" disabled selected hidden>AM/PM</option> 
+				<option value='AM'>AM</option>
+				<option value='PM'>PM</option>
+			</select>
+			</div>
+
+			<!--end time-->
+		<p class="mb-0 mb-lg-2 mt-3" style="display:inline"><strong>End Time</strong></p>
+			<div class="col-lg-auto col-12 mt-0">
+			<select class="form-control mt-2 mt-lg-0" id='endtHr' style="appearance:listbox;">
+				<option value="" disabled selected hidden>Hour</option> 
+				<option value='1'>01</option>
+				<option value='2'>02</option>
+				<option value='3'>03</option>
+				<option value='4'>04</option>
+				<option value='5'>05</option>
+				<option value='6'>06</option>
+				<option value='7'>07</option>
+				<option value='8'>08</option>
+				<option value='9'>09</option>
+				<option value='10'>10</option>
+				<option value='11'>11</option>
+				<option value='12'>12</option>
+			</select>
+			</div>
+
+			<div class="col-lg-auto col-12 mt-0">
+			<select class="form-control mt-2 mt-lg-0" id='endMin' style="appearance:listbox;">
+				<option value="" disabled selected hidden>Minute</option> 
+				<option value='00'>00</option>
+				<option value='15'>15</option>
+				<option value='30'>30</option>
+				<option value='45'>45</option>
+			</select>
+			</div>
+
+			<div class="col-lg-auto col-12 mt-0">
+			<select class="form-control mt-2 mt-lg-0" id='endAM-PM' style="appearance:listbox;">
+				<option value="" disabled selected hidden>AM/PM</option> 
+				<option value='AM'>AM</option>
+				<option value='PM'>PM</option>
+			</select>
+			</div>
+
+    </div> <!--row ends-->
+</form>
 </div>
 <!-- Bootstrap JS Bundle with Popper **needed for collapsable nav** -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
