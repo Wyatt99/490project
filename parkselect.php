c150042cb154c540c39f0dee22e93e429fefdc3f
Alex Gachassin <alexgachassin@gmail.com>
2022-10-10 15:59:02 -0500

    set team identifier as $_SESSION[team]

--- parkselect.php
+++ parkselect.php
@@ -2,6 +2,11 @@
 include 'db.php';
 include 'admin-nav.php';
 ensure_logged_in();
+
+if (!isset($_SESSION['team'])) {
+    $_SESSION['team'] = $_GET['team'];
+}
+
 ?>
 
 <!--php ends-->
@@ -35,13 +40,13 @@ ensure_logged_in();
     <div class="row">
         <div class="col mb-2 centerContent">
             <div class="card" style="width: 25rem;">
-            <a href="moore-scheduler.php"><img class="card-img" src='images/moorepark.jpg' alt="Card image cap"></a>
+            <a href="scheduler.php?team=<?=$_SESSION['team']?>&id=1"><img class="card-img" src='images/moorepark.jpg' alt="Card image cap"></a>
             </div>
         </div> <!--col end-->
 
         <div class="col centerContent">
             <div class="card" style="width: 25rem;">
-            <a href="ysc-scheduler.php"><img class="card-img" src='images/ysc.jpg' alt="Card image cap"></a>
+            <a href="scheduler.php?team=<?=$_SESSION['team']?>&id=2"><img class="card-img" src='images/ysc.jpg' alt="Card image cap"></a>
             </div>
         </div> <!--col end-->
     </div> <!--row end-->
