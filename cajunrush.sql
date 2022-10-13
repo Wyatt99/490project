1cf1f24462ce67a4319d54fcfc468ab9011a4c26
Alex Gachassin <alexgachassin@gmail.com>
2022-10-13 17:30:07 -0500

    added inserts for parks and dummy data for fields

--- cajunrush.sql
+++ cajunrush.sql
@@ -24,7 +24,7 @@ CREATE TABLE teamLocation (
 
 CREATE TABLE Team (
     teamId int NOT NULL,
-	  teamIdentifier VARCHAR(60),
+	teamIdentifier VARCHAR(60),
     teamName VARCHAR(60) NOT NULL,
     coachFirstName VARCHAR(60) NOT NULL,
     coachLastName VARCHAR(60) NOT NULL,
@@ -83,6 +83,7 @@ VALUES ('9');
 INSERT INTO ageGroup (ageGroup)
 VALUES ('10');
 
+
 INSERT INTO teamLocation (teamLocation)
 VALUES ('NS');
 INSERT INTO teamLocation (teamLocation)
@@ -91,7 +92,6 @@ INSERT INTO teamLocation (teamLocation)
 VALUES ('RS');
 INSERT INTO teamLocation (teamLocation)
 VALUES ('PR');
-
 
 INSERT INTO park (parkId, parkName)
 VALUES ('1', 'Moore Park'), ('2', 'Youngsville Sports Complex');
@@ -106,12 +106,8 @@ VALUES  ('field 1', 1, 1),
         ('field 3', 2, 1),
         ('field 4', 2, 0);
 
-
 insert into Admins (username, password)
 values ('lane', '$2y$10$FXMGOs9k2HLSRgCqH4FFGuxYMS1qXYGURLW5n4x.T01c9DlmRSfa6');
 
 INSERT INTO Season (seasonId, seasonStatus)
-VALUES ('FALL 2022', '1');
-
-INSERT INTO park (parkId, parkName) 
-VALUES (1, 'Moore Park'), (2,'Youngsville Sports Complex');
+values ('FALL 2022', '1');
