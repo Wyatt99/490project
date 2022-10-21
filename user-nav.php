ae92392e76df3ce94fdedf45884b0e262e6abe0e
Alex Gachassin <alexgachassin@gmail.com>
2022-10-21 12:18:29 -0500

    separate USER nav bar

---
+++ user-nav.php
@@ -0,0 +1,25 @@
+<header>    
+	<!--navigation bar start-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg dark">
+        <div class="container-fluid">
+            <a href="admin-home.php" class="navbar-brand"><img src="images\Cajun Rush Logo-white.png" width="auto" height="30px"></a>
+
+            <!--navbar toggle icon-->
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!--Collapsable Menu-->
+            <div class="collapse navbar-collapse " id="navbarCollapse">
+                <div class="navbar-nav">
+                    <a href="index.php" class="nav-item nav-link">Home</a>
+                      <a href="view-schedule.php" class="nav-item nav-link">View Schedules</a>
+                </div>
+                <div class="navbar-nav ms-auto">         
+                    <a href="login.php" class="nav-item nav-link">Admin Login</a>     	
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!--navigation bar end-->
+</header>
