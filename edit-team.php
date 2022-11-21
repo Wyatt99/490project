da0752ad291f7de31c9c406d2e49b0efcf86c788
Alex Gachassin <alexgachassin@gmail.com>
2022-11-21 16:02:44 -0600

    fixed footer bug

--- edit-team.php
+++ edit-team.php
@@ -87,12 +87,8 @@ if (isset($_POST["Return"])){
     ?>
 <!-- END OF PHP -->
 
-
-
-
 <!-- START OF BODY -->
 <body>
-
 <div class="container mt-3 centerContent">
 <div class="col-lg-4">
 
@@ -156,5 +152,5 @@ if (isset($_POST["Return"])){
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 <!-- END OF BODY -->
-<footer class="centerContent mt-3">Copyright &copy 2022 Cajun Rush Soccer Club</footer>
+<footer class="centerContent mt-3 editFooter">Copyright &copy 2022 Cajun Rush Soccer Club</footer>
 </html>
