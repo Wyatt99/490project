639d591dd32449bdf897f86e5d99206b55b2d0ff
Alex Gachassin <alexgachassin@gmail.com>
2022-11-21 15:26:08 -0600

    updated title

--- team-select.php
+++ team-select.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Select Team</title>
+	<title>Schedule Practices</title>
 
 	<!--Open Sans Font-->
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
